4452c337fbdf35d846d8446224fcd66535c22f47
Matthew Phillips <phillips321@gmail.com>
2018-05-08 22:23:59 +0100

    v1.6 - Added Get-FunctionalLevel and krbtgt password last changed check

--- AdAudit.ps1
+++ AdAudit.ps1
@@ -1,6 +1,7 @@
 <#
 phillips321.co.uk ADAudit.ps1
 Changlog:
+    v1.6 - Added Get-FunctionalLevel and krbtgt password last changed check
     v1.5 - Added Get-HostDetails to output simple info like username, hostname, etc...
     v1.4 - Added Get-WinVersion version to assist with some checks (SMBv1 currently)
     v1.3 - Added XML output for GPO (for offline processing using grouper https://github.com/l0ss/Grouper/blob/master/grouper.psm1)
@@ -8,34 +9,22 @@ Changlog:
     v1.1 - Fixed bug where SYSVOL research returns empty
     v1.0 - First release
 ToDo:
+  Trusts without domain filtering
+  Accounts with sid history matching the domain
+  Inactive domain trusts
+  Native [Administrator] account recently used
+  Schema Admins group not empty
+  Last change of kerberos account password
+  DCs with null session Enabled
   DCs not owned by Domain Admins: Get-ADComputer -server fruit.com -LDAPFilter "(&(objectCategory=computer)(|(primarygroupid=521)(primarygroupid=516)))" -properties name, ntsecuritydescriptor | select name,{$_.ntsecuritydescriptor.Owner}
 #>
-$versionnum = "v1.5"
-$outputdir = (Get-Item -Path ".\").FullName + "\" + $env:computername
-$starttime = get-date
-if (!(Test-Path "$outputdir")) { New-Item -ItemType directory -Path $outputdir | out-null }
-
+$versionnum = "v1.6"
 function Write-Both(){#writes to console screen and output file
     Write-Host "$args"; Add-Content -Path "$outputdir\consolelog.txt" -Value "$args"}
-
-
-Write-Both " _____ ____     _____       _ _ _
-|  _  |    \   |  _  |_ _ _| |_| |_
-|     |  |  |  |     | | | . | |  _|
-|__|__|____/   |__|__|___|___|_|_|
-$versionnum                  by phillips321
-"
-Write-Both "[*] Script start time $starttime"
-if (Get-Module -ListAvailable -Name ActiveDirectory){import-module ActiveDirectory} else {write-host "[!] ActiveDirectory module not installed, exiting..." ; exit}
-if (Get-Module -ListAvailable -Name ServerManager){import-module ServerManager} else {write-host "[!] ServerManager module not installed, exiting..." ; exit}
-if (Get-Module -ListAvailable -Name GroupPolicy){import-module GroupPolicy} else {write-host "[!] GroupPolicy module not installed, exiting..." ; exit}
-write-host "[+] Outputting to $outputdir"
-
 function Get-MachineAccountQuota{#get number of machines a user can add to a domain
     $MachineAccountQuota = (Get-ADDomain | select -exp DistinguishedName | get-adobject -prop 'ms-DS-MachineAccountQuota' | select -exp ms-DS-MachineAccountQuota)
     if ($MachineAccountQuota -gt 0){ Write-Both "    [!] Domain users can add $MachineAccountQuota devices to the domain!" }
 }
-
 function Get-PasswordPolicy{
     if (!(Get-ADDefaultDomainPasswordPolicy).PasswordComplexity) { Write-Both "    [!] Password Complexity not enabled" }
     if ((Get-ADDefaultDomainPasswordPolicy).LockoutThreshold -lt 5) {Write-Both "    [!] Lockout threshold is less than 5, currently set to $((Get-ADDefaultDomainPasswordPolicy).LockoutThreshold)" }
@@ -45,19 +34,16 @@ function Get-PasswordPolicy{
     if ((Get-ADDefaultDomainPasswordPolicy).PasswordHistoryCount -lt 12) {Write-Both "    [!] Passwords history is less than 12, currently set to $((Get-ADDefaultDomainPasswordPolicy).PasswordHistoryCount)" }
     if ((Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa).NoLmHash -eq 0) {Write-Both "    [!] LM Hashes are stored!" }
 }
-
 function Get-DomainTrusts{#lists domain trusts if they are bad
     ForEach ($trust in (Get-ADObject -Filter {objectClass -eq "trustedDomain"} -Properties TrustPartner,TrustDirection,trustType)){
         if ($trust.TrustDirection -eq 2){Write-Both "    [!] The domain $($trust.Name) is trusted by us!" }
         if ($trust.TrustDirection -eq 3){Write-Both "    [!] Bidirectyional trust with domain $($trust.Name)!" }
     }
 }
-
 function Get-WinVersion(){
     $WinVersion = [single]([string][environment]::OSVersion.Version.Major + "." + [string][environment]::OSVersion.Version.Minor)
     return [single]$WinVersion
 }
-
 function Get-SMB1Support{#check if server supports SMBv1
     if ([single](Get-WinVersion) -le [single]6.1){# NT6.1 or less detected so checking reg key
         if (!(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters).SMB1 -eq 0){Write-Both "    [!] SMBv1 is not disabled"}
@@ -66,7 +52,6 @@ function Get-SMB1Support{#check if server supports SMBv1
         if ((Get-SmbServerConfiguration).EnableSMB1Protocol){Write-Both "    [!] SMBv1 is enabled!"}
     }
 }
-
 function Get-UserPasswordNotChangedRecently{#Reports users that haven't changed passwords in more than 90 days
     $count = 0
     $DaysAgo=(Get-Date).AddDays(-90)
@@ -76,8 +61,9 @@ function Get-UserPasswordNotChangedRecently{#Reports users that haven't changed 
         $count++
     }
     if ($count -gt 0){Write-Both "    [!] $count accounts with passwords older than 90days, see accounts_with_old_passwords.txt"}
+    $krbtgtPasswordDate = (get-aduser -Filter {samaccountname -eq "krbtgt"} -Properties PasswordLastSet).PasswordLastSet 
+    if ($krbtgtPasswordDate -lt (Get-Date).AddDays(-180)){Write-Both "    [!] krbtgt password not changed since $krbtgtPasswordDate!"}
 }
-
 function Get-GPOtoFile{#oututs complete GPO report
     if (Test-Path "$outputdir\GPOReport.html") { Remove-Item "$outputdir\GPOReport.html" -Recurse; }
     Get-GPOReport -All -ReportType HTML -Path "$outputdir\GPOReport.html"
@@ -87,7 +73,6 @@ function Get-GPOtoFile{#oututs complete GPO report
     Write-Both "    [+] GPO Report saved to GPOReport.xml, now run Grouper offline using the following command"
     Write-Both "    [+]     PS>Import-Module Grouper.psm1 ; Invoke-AuditGPOReport -Path C:\GPOReport.xml -Level 3"
 }
-
 function Get-GPOsPerOU{#Lists all OUs and which GPOs apply to them
     foreach ($ouobject in Get-ADOrganizationalUnit -Filter *){
         $combinedgpos = ($(((Get-GPInheritance -Target $ouobject).InheritedGpoLinks) | select DisplayName) | ForEach-Object { $_.DisplayName }) -join ','
@@ -95,7 +80,6 @@ function Get-GPOsPerOU{#Lists all OUs and which GPOs apply to them
    }
    Write-Both "    [+] Inhertied GPOs saved to ous_inheritedGPOs.txt"
 }
-
 function Get-NTDSdit{#dumps NTDS.dit, SYSTEM and SAM for password cracking
     if (Test-Path "$outputdir\ntds.dit") { Remove-Item "$outputdir\ntds.dit" -Recurse; }
     $outputdirntds='\"' + $outputdir + '\ntds.dit\"'
@@ -104,7 +88,6 @@ function Get-NTDSdit{#dumps NTDS.dit, SYSTEM and SAM for password cracking
     Write-Both "    [+] NTDS.dit, SYSTEM & SAM saved to output folder"
     Write-Both "    [+] Use secretsdump.py -system registry/SYSTEM -ntds Active\ Directory/ntds.dit LOCAL -outputfile customer"
 }
-
 function Get-SYSVOLXMLS{#finds XML files in SYSVOL (thanks --> https://github.com/PowerShellMafia/PowerSploit/blob/master/Exfiltration/Get-GPPPassword.ps1)
     $XMLFiles = Get-ChildItem -Path "\\$Env:USERDNSDOMAIN\SYSVOL" -Recurse -ErrorAction SilentlyContinue -Include 'Groups.xml','Services.xml','Scheduledtasks.xml','DataSources.xml','Printers.xml','Drives.xml'
     if ($XMLFiles){
@@ -135,7 +118,6 @@ function Get-InactiveAccounts{#lists accounts not used in past 180 days
     }
     if ($count -gt 0){Write-Both "    [!] $count inactive user accounts(180days), see accounts_inactive.txt"}
 }
-
 function Get-DisabledAccounts{#lists disabled accounts
     $count = 0
     ForEach ($account in (Search-ADaccount -AccountInactive -Timespan 180 -UsersOnly)){
@@ -147,7 +129,6 @@ function Get-DisabledAccounts{#lists disabled accounts
     }
     if ($count -gt 0){Write-Both "    [!] $count disabled user accounts, see accounts_disabled.txt"}
 }
-
 function Get-AccountPassDontExpire{#lists accounts who's passwords dont expire
     $count = 0
     ForEach ($account in (Search-ADAccount -PasswordNeverExpires -UsersOnly)){
@@ -156,7 +137,6 @@ function Get-AccountPassDontExpire{#lists accounts who's passwords dont expire
     }
     if ($count -gt 0){Write-Both "    [!] There are $count accounts that don't expire, see accounts_passdontexpire.txt"}
 }
-
 function Get-OldBoxes{#lists server 2003/XP machines
     $count = 0
     ForEach ($machine in (Get-ADComputer -Filter {OperatingSystem -Like "*2003*" -or OperatingSystem -Like "*XP*"} -Property *)){
@@ -171,15 +151,46 @@ function Get-HostDetails{#gets basic information about the host
     Write-Both "    [+] User Name:  $env:UserName"
     Write-Both "    [+] NT Version:  $(Get-WinVersion)"
     $IPAddresses = [net.dns]::GetHostAddresses("")|Select -Expa IP*
-    ForEach ($ip in $IPAddresses){Write-Both "    [+] IP Address:  $ip"}  
+    ForEach ($ip in $IPAddresses){Write-Both "    [+] IP Address:  $ip"}
 }
-
+function Get-FunctionalLevel{# Gets the functional level for domain and forest
+    $DomainLevel = (Get-ADDomain).domainMode
+    if ($DomainLevel -eq "Windows2000Domain" -and [single](Get-WinVersion) -gt 5.0){Write-Both "    [!] DomainLevel is reduced for backwards compatibility to $DomainLevel!"}
+    if ($DomainLevel -eq "Windows2003InterimDomain" -and [single](Get-WinVersion) -gt 5.1){Write-Both "    [!] DomainLevel is reduced for backwards compatibility to $DomainLevel!"}
+    if ($DomainLevel -eq "Windows2003Domain" -and [single](Get-WinVersion) -gt 5.2){Write-Both "    [!] DomainLevel is reduced for backwards compatibility to $DomainLevel!"}
+    if ($DomainLevel -eq "Windows2008Domain" -and [single](Get-WinVersion) -gt 6.0){Write-Both "    [!] DomainLevel is reduced for backwards compatibility to $DomainLevel!"}
+    if ($DomainLevel -eq "Windows2008R2Domain" -and [single](Get-WinVersion) -gt 6.1){Write-Both "    [!] DomainLevel is reduced for backwards compatibility to $DomainLevel!"}
+    if ($DomainLevel -eq "Windows2012Domain" -and [single](Get-WinVersion) -gt 6.2){Write-Both "    [!] DomainLevel is reduced for backwards compatibility to $DomainLevel!"}
+    if ($DomainLevel -eq "Windows2012R2Domain" -and [single](Get-WinVersion) -gt 6.3){Write-Both "    [!] DomainLevel is reduced for backwards compatibility to $DomainLevel!"}
+    $ForestLevel = (Get-ADForest).ForestMode
+    if ($ForestLevel -eq "Windows2000Forest" -and [single](Get-WinVersion) -gt 5.0){Write-Both "    [!] ForestLevel is reduced for backwards compatibility to $ForestLevel!"}
+    if ($ForestLevel -eq "Windows2003InterimForest" -and [single](Get-WinVersion) -gt 5.1){Write-Both "    [!] ForestLevel is reduced for backwards compatibility to $ForestLevel!"}
+    if ($ForestLevel -eq "Windows2003Forest" -and [single](Get-WinVersion) -gt 5.2){Write-Both "    [!] ForestLevel is reduced for backwards compatibility to $ForestLevel!"}
+    if ($ForestLevel -eq "Windows2008Forest" -and [single](Get-WinVersion) -gt 6.0){Write-Both "    [!] ForestLevel is reduced for backwards compatibility to $ForestLevel!"}
+    if ($ForestLevel -eq "Windows2008R2Forest" -and [single](Get-WinVersion) -gt 6.1){Write-Both "    [!] ForestLevel is reduced for backwards compatibility to $ForestLevel!"}
+    if ($ForestLevel -eq "Windows2012Forest" -and [single](Get-WinVersion) -gt 6.2){Write-Both "    [!] ForestLevel is reduced for backwards compatibility to $ForestLevel!"}
+    if ($ForestLevel -eq "Windows2012R2Forest" -and [single](Get-WinVersion) -gt 6.3){Write-Both "    [!] ForestLevel is reduced for backwards compatibility to $ForestLevel!"}
+}
+$outputdir = (Get-Item -Path ".\").FullName + "\" + $env:computername
+$starttime = get-date
+if (!(Test-Path "$outputdir")) { New-Item -ItemType directory -Path $outputdir | out-null }
+Write-Both " _____ ____     _____       _ _ _
+|  _  |    \   |  _  |_ _ _| |_| |_
+|     |  |  |  |     | | | . | |  _|
+|__|__|____/   |__|__|___|___|_|_|
+$versionnum                  by phillips321
+"
+Write-Both "[*] Script start time $starttime"
+if (Get-Module -ListAvailable -Name ActiveDirectory){import-module ActiveDirectory} else {write-host "[!] ActiveDirectory module not installed, exiting..." ; exit}
+if (Get-Module -ListAvailable -Name ServerManager){import-module ServerManager} else {write-host "[!] ServerManager module not installed, exiting..." ; exit}
+if (Get-Module -ListAvailable -Name GroupPolicy){import-module GroupPolicy} else {write-host "[!] GroupPolicy module not installed, exiting..." ; exit}
+write-host "[+] Outputting to $outputdir"
 Write-Both "[*] Device Information" ; Get-HostDetails
 Write-Both "[*] Password Policy Findings" ; Get-PasswordPolicy ; Get-UserPasswordNotChangedRecently
 Write-Both "[*] Looking for accounts that dont expire" ; Get-AccountPassDontExpire
 Write-Both "[*] Looking for inactive/disabled accounts" ; Get-InactiveAccounts ; Get-DisabledAccounts
 Write-Both "[*] Looking for server 2003/XP machines connected to domain" ; Get-OldBoxes
-Write-Both "[*] AD Findings" ; Get-MachineAccountQuota ; Get-SMB1Support
+Write-Both "[*] AD Findings" ; Get-MachineAccountQuota ; Get-SMB1Support; Get-FunctionalLevel
 Write-Both "[*] Domain Trust Findings" ; Get-DomainTrusts
 Write-Both "[*] GPO Findings"  ; Get-GPOtoFile ; Get-GPOsPerOU
 Write-Both "[*] Trying to find SysVOL xml files containg cpassword..."; Get-SYSVOLXMLS
