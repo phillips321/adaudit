7f7f8c4e20bc04743340e0298e13fb8c616e893a
Cool34000 <17827397+Cool34000@users.noreply.github.com>
2022-01-26 17:41:51 +0100

    Update AdAudit.ps1

--- AdAudit.ps1
+++ AdAudit.ps1
@@ -1,7 +1,7 @@
 <#
 phillips321.co.uk ADAudit.ps1
 Changelog:
-    v5.2 - Enhanced Get-LAPSStatus. Added news checks (AD services + Windows Update + NTP source). Added support for WS 2022. Fix OS version difference check for WS 2008
+    v5.2 - Enhanced Get-LAPSStatus. Added news checks (AD services + Windows Update + NTP source). Added support for WS 2022. Fix OS version difference check for WS 2008.
     v5.1 - Added check for newly created users and groups. Added check for replication mechanism. Added check for Recycle Bin. Fix ProtectedUsers for WS 2008.
     v5.0 - Make the script compatible with other language than English. Fix the cpassword search in GPO. Fix Get-ACL bad syntax error. Fix Get-DNSZoneInsecure for WS 2008.
     v4.9 - Bug fix in checking password comlexity
@@ -333,7 +333,7 @@ Function Get-UserPasswordNotChangedRecently{#Reports users that haven't changed 
         Write-Nessus-Finding "krbtgtPasswordNotChanged" "KB253" "krbtgt password not changed since $krbtgtPasswordDate"
     }
 }
-Function Get-GPOtoFile{#Oututs complete GPO report
+Function Get-GPOtoFile{#Outputs complete GPO report
     if (Test-Path "$outputdir\GPOReport.html") { Remove-Item "$outputdir\GPOReport.html" -Recurse; }
     Get-GPOReport -All -ReportType HTML -Path "$outputdir\GPOReport.html"
     Write-Both "    [+] GPO Report saved to GPOReport.html"
